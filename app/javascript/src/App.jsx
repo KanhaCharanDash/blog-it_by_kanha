c52fb0a5af5c2b4977dda50b4aa06e68c16a8fa6
Kanha Charan Dash <kanhacharandash111@gmail.com>
2025-07-07 15:18:08 +0530

    Added API based architecture

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -5,8 +5,8 @@ import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 const App = () => (
   <Router>
     <Switch>
-      <Route exact path="/" render={() => <div>Home</div>} />
-      <Route exact path="/about" render={() => <div>About</div>} />
+      {/* <Route exact path="/" render={() => <div>Home</div>} /> */}
+      <Route exact path="/" render={() => <div>About</div>} />
     </Switch>
   </Router>
 );
