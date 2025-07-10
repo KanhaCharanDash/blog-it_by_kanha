2a8ba2e46d306969dfeec255114782ecbd38f6b7
Kanha Charan Dash <kanhacharandash111@gmail.com>
2025-07-11 00:05:33 +0530

    Added slug to task

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   get "posts/index"
-  resources :posts, only: [:index]
+  resources :posts, only: [:index], param: :slug
   root "home#index"
   get "*path", to: "home#index", via: :all
 end
