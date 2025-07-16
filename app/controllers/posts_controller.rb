5ea28e6feac62795444030289ba3ffe8b12f4bdd
Kanha Charan Dash <kanhacharandash111@gmail.com>
2025-07-16 11:11:20 +0530

    added fetch post logic using organization id

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,20 +2,23 @@
 
 class PostsController < ApplicationController
   def index
-    posts = Post.includes(:categories, :user, :organization).all
+    posts = Post.includes(:categories, :user, :organization)
+      .where(organization_id: current_user.organization_id)
 
     render json: {
       posts: posts.map do |post|
         post.as_json(
-          only: [:id, :title, :description, :slug, :created_at], include: {
+          only: [:id, :title, :description, :slug, :created_at],
+          include: {
             categories: { only: [:id, :name] }
-          }).merge(
-            author_name: post.user.name,
-            organization_name: post.organization.name
+          }
+        ).merge(
+          author_name: post.user.name,
+          organization_name: post.organization.name
         )
       end
     }
- end
+  end
 
   def create
     user = User.find(params[:user_id])
@@ -34,7 +37,7 @@ class PostsController < ApplicationController
     else
       render json: { errors: post.errors.full_messages }, status: :unprocessable_entity
     end
- end
+   end
 
   def show
     post = Post.includes(:categories, :user, :organization).find_by!(slug: params[:slug])
