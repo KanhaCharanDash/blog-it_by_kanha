868e90c73a84e0755dd174d94bb742dfe288b0d8
Kanha Charan Dash <kanhacharandash111@gmail.com>
2025-07-18 00:24:25 +0530

    Initialized query client instance

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 
 import { either, isEmpty, isNil } from "ramda";
+import { QueryClientProvider } from "react-query";
 import {
   BrowserRouter as Router,
   Route,
@@ -20,58 +21,61 @@ import Edit from "./components/posts/Edit";
 import NewPost from "./components/posts/NewPost";
 import Show from "./components/posts/Show";
 import useAuthStore from "./components/stores/useAuthStore";
+import queryClient from "./utils/queryClient";
 
 const App = () => {
   const authToken = useAuthStore(state => state.authToken);
   const isLoggedIn = !either(isNil, isEmpty)(authToken);
 
   return (
-    <Router>
-      <ToastContainer />
-      <Switch>
-        <Route exact component={Signup} path="/signup" />
-        <Route exact component={Login} path="/login" />
-        <PrivateRoute
-          exact
-          component={Posts}
-          condition={isLoggedIn}
-          path="/posts"
-          redirectRoute="/login"
-        />
-        <PrivateRoute
-          exact
-          component={NewPost}
-          condition={isLoggedIn}
-          path="/posts/new"
-          redirectRoute="/login"
-        />
-        <PrivateRoute
-          exact
-          component={Show}
-          condition={isLoggedIn}
-          path="/posts/:slug"
-          redirectRoute="/login"
-        />
-        <PrivateRoute
-          exact
-          component={Edit}
-          condition={isLoggedIn}
-          path="/posts/:slug/edit"
-          redirectRoute="/login"
-        />
-        <PrivateRoute
-          exact
-          component={BlogPosts}
-          condition={isLoggedIn}
-          path="/my-posts"
-          redirectRoute="/login"
-        />
-        <Route exact path="/">
-          <Redirect to="/posts" />
-        </Route>
-        <Route exact component={NoDataPage} path="*" />
-      </Switch>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <ToastContainer />
+        <Switch>
+          <Route exact component={Signup} path="/signup" />
+          <Route exact component={Login} path="/login" />
+          <PrivateRoute
+            exact
+            component={Posts}
+            condition={isLoggedIn}
+            path="/posts"
+            redirectRoute="/login"
+          />
+          <PrivateRoute
+            exact
+            component={NewPost}
+            condition={isLoggedIn}
+            path="/posts/new"
+            redirectRoute="/login"
+          />
+          <PrivateRoute
+            exact
+            component={Show}
+            condition={isLoggedIn}
+            path="/posts/:slug"
+            redirectRoute="/login"
+          />
+          <PrivateRoute
+            exact
+            component={Edit}
+            condition={isLoggedIn}
+            path="/posts/:slug/edit"
+            redirectRoute="/login"
+          />
+          <PrivateRoute
+            exact
+            component={BlogPosts}
+            condition={isLoggedIn}
+            path="/my-posts"
+            redirectRoute="/login"
+          />
+          <Route exact path="/">
+            <Redirect to="/posts" />
+          </Route>
+          <Route exact component={NoDataPage} path="*" />
+        </Switch>
+      </Router>
+    </QueryClientProvider>
   );
 };
 
